38abc54cb0b7ce8da56346e90df6e374a7daca44
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-02-20 16:21:44 -0300

    feat: created busStationData

---
+++ src/types/locationTypes.ts
@@ -0,0 +1,35 @@
+export type BusStationData = {
+  geometry: {
+    location: {
+      lat: number;
+      lng: number;
+    };
+    viewport: {
+      northeast: {
+        lat: number;
+        lng: number;
+      };
+      southwest: {
+        lat: number;
+        lng: number;
+      };
+    };
+  };
+  icon: string;
+  icon_background_color: string;
+  icon_mask_base_uri: string;
+  name: string;
+  photos: [
+    {
+      height: number;
+      html_attributions: [string];
+      photo_reference: string;
+      width: number;
+    }
+  ];
+  place_id: string;
+  reference: string;
+  scope: string;
+  types: string[];
+  vicinity: string;
+};
