9404b23539e84867c2f4d9f4f209447b9e22c298
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-02-21 14:31:31 -0300

    feat: styled the switch button

--- src/components/MyButton.tsx
+++ src/components/MyButton.tsx
@@ -1,4 +1,4 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { useMutation } from "react-query";
 import { requestNearbyBusStation } from "../services/locationService";
 import { BusStationData } from "../types/locationTypes";
@@ -6,10 +6,12 @@ import { BusStationData } from "../types/locationTypes";
 export default function MyButton({
   latitude,
   longitude,
+  busStations,
   setBusStations,
 }: {
   latitude: number;
   longitude: number;
+  busStations: BusStationData[] | null;
   setBusStations: React.Dispatch<React.SetStateAction<BusStationData[] | null>>;
 }) {
   const getNearbyStation = useMutation(async () => {
@@ -17,9 +19,10 @@ export default function MyButton({
   });
 
   const showBusStations = async () => {
+    if (busStations) return setBusStations(null);
+
     try {
       const data = await getNearbyStation.mutateAsync();
-      console.log(data);
       setBusStations(data);
     } catch (error) {
       console.log(JSON.stringify(error));
@@ -27,8 +30,36 @@ export default function MyButton({
   };
 
   return (
-    <TouchableOpacity onPress={showBusStations}>
-      <Text>OPA</Text>
+    <TouchableOpacity onPress={showBusStations} style={styles.buttonContainer}>
+      <Image
+        source={require("../assets/image/logo_take_a_bus.png")}
+        style={styles.imageLogo}
+      />
     </TouchableOpacity>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    position: "absolute",
+    bottom: 20,
+    width: 80,
+    height: 80,
+    backgroundColor: "#f5ab35",
+    shadowColor: "black",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    elevation: 4,
+    padding: 8,
+    borderRadius: 50,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  imageLogo: {
+    width: 40,
+    height: 40,
+  },
+});
